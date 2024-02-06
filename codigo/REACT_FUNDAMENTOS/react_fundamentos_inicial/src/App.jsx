691be0317348f68507d243b2ef4400b6db82f89a
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-02-06 11:27:39 +0100

    Archivos Primer Proyecto
    Se añaden todos los archivos necesarios para el primer proyecto.

---
+++ codigo/REACT_FUNDAMENTOS/react_fundamentos_inicial/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-logo-xs.png" alt="React logo" />
+        <h1>React Fundamentos</h1>
+        <p>
+          ¡Conceptos fundamentales de React que necesitas conocer para desarrollar cualquier app con esta famosa librería!
+        </p>
+      </header>
+
+      <main>
+        <h2>¡Comenzamos Picando Código en React!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
