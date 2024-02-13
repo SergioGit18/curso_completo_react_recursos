e3f45d10e61e75464fe7de46f68164cdcd257bdb
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-02-13 17:49:53 +0100

    Añadiendo los ficheros para el tema rutas dinamicas

---
+++ codigo/REACT_FUNDAMENTOS/04-react_fundamentos_rutas_dinamicas/src/App.jsx
@@ -0,0 +1,37 @@
+import reactImage from './assets/react-logo-xs.png';
+
+const reactTitles = ['React Fundamentos', 'React Bases', 'React Principios'];
+
+function getRandomIntInRange(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function Header() {
+  const titleWinner = reactTitles[getRandomIntInRange(0, 2)];
+
+  return (
+    <header>
+      <img src={reactImage} alt="React logo" />
+      <h1>{titleWinner}</h1>
+      {/* De esta forma podríamos hacerlo directamente en una línea
+      <h1>{reactTitles[getRandomIntInRange(0, 2)]}</h1> */}
+      <p>
+        ¡Conceptos fundamentales de React que necesitas conocer para desarrollar
+        cualquier app con esta famosa librería!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <h2>¡Comenzamos Picando Código en React!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
