3405404b448ac756f70dfe3aa3286a2bfa3a47c3
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-03-27 14:06:58 +0100

    Subiendo ficheros

---
+++ codigo/REACT_ESENCIALES/07-react_esenciales_creando_menus_dinamicos/src/components/ExamplesSection/ExamplesSection.jsx
@@ -0,0 +1,47 @@
+import TabButton from "../../components/TabButton/TabButton.jsx";
+import Section from "../Section/Section.jsx";
+import TabsMenu from "../TabsMenu/TabsMenu.jsx";
+import { useState } from "react";
+import { EXAMPLES } from "../../data.js";
+import "./ExamplesSection.css";
+
+export default function ExamplesSection() {
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  function handleClickMenu(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(`Ey! Estas pulsando el botón ${selectedTopic}`);
+  }
+  console.log("El componente App esta siendo renderizado y ejecutado!!");
+
+  let tabContent = (
+    <p>
+      Aquí se va a mostrar información sobre una característica de React, para
+      ello elige una opción del menú
+    </p>
+  );
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  const buttons = Object.values(EXAMPLES).map((button, index) => 
+    <TabButton key={index} onClick={() => handleClickMenu(button.key)}>{button.title}</TabButton>
+  );
+
+  return (
+    <Section title="Ejemplos React" id="reactExamples" className="miClase">
+      <TabsMenu buttons={<>{buttons}</>}>
+        {tabContent}
+      </TabsMenu>
+      <menu></menu>
+    </Section>
+  );
+}
