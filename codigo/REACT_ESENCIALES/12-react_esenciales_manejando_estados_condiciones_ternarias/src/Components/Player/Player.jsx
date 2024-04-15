c1fdcc88826c0d2798da302b7094224739b3cef2
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-04-15 16:10:29 +0200

    Add files via upload

---
+++ codigo/REACT_ESENCIALES/12-react_esenciales_manejando_estados_condiciones_ternarias/src/Components/Player/Player.jsx
@@ -0,0 +1,32 @@
+import './Player.css';
+import { useState } from 'react';
+
+export default function Player({playerName, playerSymbol}) {
+
+  const [isEditing,setIsEditing] = useState(false);
+
+  function handleChangeClick(){
+    setIsEditing(!isEditing);
+  }
+
+  const playerNameField = isEditing 
+  ? (<input type="text" name="" id="" required/>)
+  : (<span className="player-name">{playerName}</span>);
+
+  const buttonChangeValue = isEditing 
+  ? ("Guardar")
+  : ("Cambiar");
+
+  return (
+    <>
+      <li>
+        
+        <span className="player">
+          {playerNameField}
+          <span className="player-symbol">{playerSymbol}</span>
+        </span>
+        <button onClick={()=>handleChangeClick()}>{buttonChangeValue}</button>
+      </li>
+    </>
+  );
+}
