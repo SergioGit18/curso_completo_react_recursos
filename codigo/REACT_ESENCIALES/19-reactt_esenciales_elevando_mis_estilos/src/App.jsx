b95f9e60fb9da552837c7aaaee8030e49cd8e463
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-04-30 15:19:32 +0200

    Subiendo ficheros

---
+++ codigo/REACT_ESENCIALES/19-reactt_esenciales_elevando_mis_estilos/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import Player from "./Components/Player/Player.jsx";
+import GameBoard from "./Components/GameBoard/GameBoard.jsx";
+
+import { useState } from 'react';
+
+function App() {
+
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handlePickedSquare(){
+    console.log("Ahora mismo tiene el turno el jugador que utiliza el símbolo: "+activePlayer);
+    setActivePlayer((lastActivePlayer)=> (activePlayer === 'X') ? 'O' : 'X');
+    console.log("Hemos cambiado el turno, ahora el turno lo tiene el jugador que utiliza el símbolo: "+activePlayer);
+  }
+
+  return (
+    <>
+      <main>
+        <div id="game-container">
+          <ol id="playersContainer" className="highlight-player">
+            <Player initialName="Jugador 1" playerSymbol="X" isActive={activePlayer === 'X'}></Player>        
+            <Player initialName="Jugador 2" playerSymbol="O" isActive={activePlayer === 'O'}></Player>                
+          </ol>
+          <GameBoard activePlayerSymbol={activePlayer} onSelectSquare={()=>handlePickedSquare()}/>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ codigo/REACT_ESENCIALES/19-reactt_esenciales_elevando_mis_estilos/src/Components/Player/Player.jsx
@@ -0,0 +1,39 @@
+import './Player.css';
+import { useState } from 'react';
+
+export default function Player({initialName, playerSymbol, isActive}) {
+
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing,setIsEditing] = useState(false);
+
+  function handleChangeClick(){
+    // Forma correcta
+    setIsEditing((isEditing)=> !isEditing);
+    //Forma incorrecta
+    // setIsEditing(!isEditing);
+  }
+
+  function handleNameChange(event){
+    setPlayerName(event.target.value);
+  }
+
+  const playerNameField = isEditing 
+  ? (<input type="text" name="" id="" required value={playerName} onChange={(event)=>handleNameChange(event)}/>)
+  : (<span className="player-name">{playerName}</span>);
+
+  const buttonChangeValue = isEditing 
+  ? ("Guardar")
+  : ("Cambiar");
+
+  return (
+    <>
+      <li className={isActive ? 'active': undefined}>    
+        <span className="player">
+          {playerNameField}
+          <span className="player-symbol">{playerSymbol}</span>
+        </span>
+        <button onClick={()=>handleChangeClick()}>{buttonChangeValue}</button>
+      </li>
+    </>
+  );
+}
