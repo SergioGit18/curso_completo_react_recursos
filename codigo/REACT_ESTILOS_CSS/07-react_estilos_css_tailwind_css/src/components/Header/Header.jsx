3dfd904acef46da42c9a91d9c58d0b0db7d459db
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-09-04 17:05:07 +0200

    Subida de ficheros necesarios

---
+++ codigo/REACT_ESTILOS_CSS/07-react_estilos_css_tailwind_css/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+import wonkaLogo from '../../assets/wonkaLogo.png';
+
+const Header = () => {
+  return (
+    <header className='flex'>
+      <img
+        src={wonkaLogo}
+        alt='WonkaLogo'
+      />
+      <div className='register'>
+        <button className='register-button'>Register</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
