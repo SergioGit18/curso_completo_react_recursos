6c0905cf17bffb0d43941b66aeabeca2b732b880
Sergio Correas Rayo (Picando Código) <sergiocorreas18@gmail.com>
2024-11-16 12:04:03 +0100

    Subiendo mas ficheros

---
+++ codigo/REACT_REFS_AND_PORTALS/07-react_refs_portals_forward_ref/src/Components/ModalDialog.jsx
@@ -0,0 +1,32 @@
+import { forwardRef } from "react";
+
+const ModalDialog = forwardRef(function ModalDialog({resultGame, timeTarget}, ref){
+    const secondsText = (timeTarget > 1 ? 'segundos' : 'segundo');
+    return (
+      <>
+        <dialog
+          ref={ref}
+          className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-none rounded-lg p-8 bg-[#d7fcf8]'>
+          <h2 className='font-mono text-center uppercase font-bold text-4xl mt-2 mb-2'>
+            ¡Has {resultGame}!
+          </h2>
+          <p className='text-lg'>
+            El contrareloj estaba pautado para finalizar en {timeTarget}{' '}
+            {secondsText}
+          </p>
+          <p>
+            Has parado el tiempo a falta de <strong>X</strong> segundos
+          </p>
+          <form action='dialog'>
+            <button
+              className='mt-4 pt-2 pb-2 pl-4 pr-4 border-none 
+            rounded-md bg-[#12352f] text-[#edfcfa] text-lg cursor-pointer hover:bg-[#051715]'>
+              Cerrar
+            </button>
+          </form>
+        </dialog>
+      </>
+    );
+})
+
+export default ModalDialog;
---
+++ codigo/REACT_REFS_AND_PORTALS/07-react_refs_portals_forward_ref/src/Components/Timer.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from "react";
+import ModalDialog from "./ModalDialog";
+
+
+const Timer = ({title, time}) =>{
+
+    const timer = useRef();
+    const dialogModal = useRef();
+
+    const [hasLooser, setHasLooser] = useState(false);
+    const [hasStart, setHasStart] = useState(false);
+
+    
+
+    const handleStartTime = () => {
+        setHasStart(true);
+        timer.current = setTimeout(()=>{
+            dialogModal.current.showModal();
+            setHasStart(false);
+            setHasLooser(true);
+        }, time * 1000)
+  
+    }
+
+    const handleStopTime = () => {
+      setHasStart(false);
+      clearInterval(timer.current);
+    }
+
+    return (
+      <>
+        <ModalDialog ref={dialogModal} resultGame="perdido" timeTarget={time}/>
+        <section
+          className='w-[22rem] flex flex-col items-center justify-center p-8 m-8 
+            bg-[#691A1A] text-[##221c18] shadow-lg rounded-md'>
+          <h2 className='text-xl tracking-wide text-center uppercase text-[#edfcfa] font-bold mb-2'>
+            {title}
+          </h2>
+          {hasLooser && <p className='my-1 mb-3'>Has perdido</p>}
+          <p className='border-[1px] border-solid border-[#46cebe] rounded px-2 py-1 m-'>
+            {time} segundo{time > 1 ? 's' : ''}
+          </p>
+          <p
+            className='mt-4 px-4 py-2 border-none rounded-md bg-[#12352f] text-[#edfcfa] 
+        text-lg cursor-pointer hover:bg-[#051715]'>
+            <button onClick={hasStart ? handleStopTime : handleStartTime}>
+              {hasStart ? 'Parar' : 'Empezar'}
+            </button>
+          </p>
+          <p className=''></p>
+        </section>
+      </>
+    );
+}
+
+export default Timer;
